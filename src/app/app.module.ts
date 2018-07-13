9a0cb50f88328aa24bc09571bccc5ef0f5e23d84
Donavin <donavinhannon@gmail.com>
2018-07-13 10:29:55 -0700

    Added bootstrap 4, initial layout for prototyping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LocationSearchComponent } from './location-search/location-search.component';
+import { WeatherCurrentComponent } from './weather-current/weather-current.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    LocationSearchComponent,
+    WeatherCurrentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/weather-current/weather-current.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-current',
+  templateUrl: './weather-current.component.html',
+  styleUrls: ['./weather-current.component.scss']
+})
+export class WeatherCurrentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
