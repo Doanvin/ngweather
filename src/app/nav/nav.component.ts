255cb0f1c726c4b720c59de085721a68134ab12b
Donavin <donavinhannon@gmail.com>
2018-07-19 14:39:36 -0700

    Added search to non-home pages

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,15 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-  private menu;
+  private menu: HTMLElement;
+  private search: HTMLElement;
+  nav_width: number;
+  wide_enough: boolean;
+  location_home: boolean;
 
   constructor() { }
 
   ngOnInit() {
     this.menu = document.getElementById('menu');
+    this.search = document.getElementById('search');
+    // doesn't change if screen width changes
+    this.nav_width = document.querySelector('.nav').getBoundingClientRect().width;
+    this.wide_enough = this.nav_width > 576;
+    this.location_home = window.location.pathname === '/';
   }
 
   toggleMenu() {
     this.menu.hidden = !this.menu.hidden;
+  }
+
+  toggleSearch() {
+    this.search.hidden = !this.search.hidden;
   }
 }
